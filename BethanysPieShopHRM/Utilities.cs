448c32eb3aa77f87c34fea492a9ad52cc87a0d5b
William Farmer <william.farmer@cardinus.com>
2024-03-26 15:50:46 +0000

    Initial commit Utilitie.cs; Complete Understanding Variable Scope

---
+++ BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+	internal class Utilities
+	{
+		public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+		{
+			//Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked}");
+			//return monthlyWage * numberOfMonthsWorked;
+
+			if (numberOfMonthsWorked == 12)//let's add a bonus month
+				return monthlyWage * (numberOfMonthsWorked + 1);
+
+			return monthlyWage * numberOfMonthsWorked;
+		}
+
+		public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+		{
+			Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+			return monthlyWage * numberOfMonthsWorked + bonus;
+		}
+
+		public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus)
+		{
+			Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+			return monthlyWage * numberOfMonthsWorked + bonus; 
+		}
+	}
+}
